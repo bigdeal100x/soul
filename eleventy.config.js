fa14466ee58923b5e8ca2cf735df7beaa36a92cf
Tiana Dueck <tianadueck@pm.me>
2026-09-17 11:56:47 -0400

    font

--- eleventy.config.js
+++ eleventy.config.js
@@ -26,7 +26,7 @@ export default async function(eleventyConfig) {
 		.addPassthroughCopy({ "./public/": "/", })
 		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl")
 		.addPassthroughCopy({ './robots.txt': '/robots.txt', })
-		.addPassthroughCopy("/fonts/Feroniapi-MediumItalic.woff2");
+		.addPassthroughCopy("./fonts/Feroniapi-MediumItalic.woff2");
 
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets

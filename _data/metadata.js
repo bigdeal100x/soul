800dfc186694cd624edcf7401aa4b2bf03c6289c
bigdeal100x <estelleh@ocadu.ca>
2026-09-17 19:41:37 -0400

    additions

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,6 +1,6 @@
 export default {
 	title: "Blogging for the Soul",
-	url: "https://example.com/",
+	url: "https://howcouldyou.netlify.app/",
 	language: "en",
 	description: "How to get into blogging by hand.",
 	author: {
